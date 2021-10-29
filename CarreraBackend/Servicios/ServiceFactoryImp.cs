7fe7959ece45efa49886dab1018720b9e49b16a6
MatiMolina22 <112837@tecnicatura.frc.utn.edu.ar>
2021-10-29 19:00:05 -0300

    Agregar IService

---
+++ CarreraBackend/Servicios/ServiceFactoryImp.cs
@@ -0,0 +1,18 @@
+﻿using CarreraBackend.Servicios.Implementaciones;
+using CarreraBackend.Servicios.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarreraBackend.Servicios
+{
+    class ServiceFactoryImp : AbstractServiceFactory
+    {
+        public override IService CrearService()
+        {
+            return new CarreraService();
+        }
+    }
+}
