a740be32b83dab3490bdd056c1406aa9f916132b
Abraguas <64881344+Abraguas@users.noreply.github.com>
2021-11-01 20:55:09 -0300

    Cambios automaticos

--- CarreraFrontend/Program.cs
+++ CarreraFrontend/Program.cs
@@ -1,3 +1,4 @@
+using CarreraFrontend.Forms;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,7 +18,7 @@ namespace CarreraFrontend
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmPrincipal());
         }
     }
 }
